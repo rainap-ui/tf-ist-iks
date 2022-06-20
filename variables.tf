8d9cffe3ec4daca304c6f8f790561ded05986b9c
rainap-ui <prashansa312@gmail.com>
2022-06-20 19:56:12 +0530

    removed creds

--- variables.tf
+++ variables.tf
@@ -1,35 +1,75 @@
 variable "apikey" {
-  type        = string
-  description = "API Key"
+    type = string
 }
+
 variable "secretkey" {
-  type        = string
-  description = "Secret Key or file location"
+    type = string
+    default = "../Secretkey.txt"
 }
 variable "endpoint" {
-  type        = string
-  description = "API Endpoint URL"
-  default     = "https://www.intersight.com"
+    type = string
+    default = "https://intersight.com"
 }
-variable "organization" {
-  type        = string
-  description = "Organization Name"
-  default     = "default"
+
+variable "iksclustername" {
+    type = string
 }
+
+variable "num-control-nodes" {
+    type = number
+}
+
+variable "num-worker-nodes" {
+    type = number
+}
+
+variable "num-load-balancer" {
+    type = number
+}
+
+variable "num-worker-nodes-max" {
+    type = number
+}
+
 variable "ssh_user" {
-  type        = string
-  description = "SSH Username for node login."
+    type = string
 }
 variable "ssh_key" {
-  type        = string
-  description = "SSH Public Key to be used to node login."
+    type = string
 }
-variable "vc_password" {
-  sensitive   = true
-  type        = string
-  description = "Password of the account to be used with vCenter.  This should be the password for the account used to register vCenter with Intersight."
+
+variable "ip_pool-name" {
+    type = string
+}
+
+variable "sysconfig-policy-name" {
+    type = string
+}
+
+variable "k8s_network-name" {
+    type = string
+}
+
+variable "k8s_version-policy-name" {
+    type = string
+}
+
+variable "k8s_version-version-name" {
+    type = string
+}
+
+variable "k8s_infra-policyName" {
+    type = string
+}
+
+variable "k8s_worker-policyName" {
+    type = string
+}
+
+variable "organization" {
+    type = string
 }
 variable "tags" {
-  type    = list(map(string))
-  default = []
+    type    = list(map(string))
 }
+
